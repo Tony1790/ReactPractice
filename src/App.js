68ce4bda2cd28e0e9d29d636fc2dbe3ceb4bbd6e
Tony1790 <tony45081790@gmail.com>
2022-10-06 13:15:02 +0900

    ToDo

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time"); //이 코드는 state가 변할 때 마다 매번 실행됨.
-  useEffect(() => {
-    console.log("I run only once");
-  }, []);
-  useEffect(() => {
-    console.log("I run when Keyword is changed");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when Counter is changed");
-  }, [counter]);
-  /* useEffect를 사용하니 state가 변화하더라도 한번만 실행. 
-    첫번째 인자는 한번만 실행, 두번째 인자는 */
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]); //어플리케이션이 시작될 때는 array가 비어있음
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]); //새롭게 작성한 toDo와 기존의 toDo가 setToDos에 저장.
+    setToDo("");
+  };
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-/*
-🔥 새롭게 배운 내용 정리
-
-• 리액트를 사용하는 이유: 최소 단위의 렌더링을 위해
-• useState(): 변수, 변수를 제어하는 함수로 구성되며 변하는 값을 제어, 해당 부분의 리렌더링을 위함
-• props: 태그의 속성 값을 함수의 아규먼트 처럼 컴포넌트에 값을 전달해준다.
-• useEffect(): 코드의 실행 시점을 관리할 수 있는 선택권을 얻는 방어막 같은 존재, 디펜던시가 없을 경우 최초 1회 실행, 있을 경우 해당 값이 변할 경우 실행한다. 이 때 디펜던시는 여러개 입력이 가능하다.
-
-🏴 부모 컴포넌트에서 리렌더링이 일어날 경우 모든 자식들이 리렌더링이 된다.(wa can use memo)
-🏴 propType을 설치하고 props의 타입을 지정해 줄 수 있다. 이 때 isRequired로 필수값을 지정 가능
-*/
 
 export default App;
