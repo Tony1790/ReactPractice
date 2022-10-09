ff0517210ba1e99a4c3ba28c604dabbf47bc5d50
Tony1790 <tony45081790@gmail.com>
2022-10-09 11:39:16 +0900

    Movie app 1_1

--- src/App.js
+++ src/App.js
@@ -1,64 +1,42 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(1);
-  const [cost, setCost] = useState(1);
-
-  /* const getCoinNames = (element) => {
-    return element.symbol;
+  const [movies, setMovie] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovie(json.data.movies);
+    setLoading(false);
   };
-  const coinName = coins.map(getCoinNames);
-  console.log(coinNames); */
-
-  const onChange = (event) => {
-    setCost(event.target.value);
-    setMoney(1);
-  };
-
-  const handleInput = (event) => setMoney(event.target.value);
-
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-
   return (
     <div>
-      <h1>The Coin! {loading ? "" : `(${coins.length})`}</h1>
-
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onChange}>
-          {coins.map((coin) => (
-            <option
-              key={coin.id}
-              value={coin.quotes.USD.price}
-              id={coin.name}
-              symbol={coin.name}
-            >
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                <li>
+                  {movie.genres.map((g) => (
+                    <li key={g}>{g}</li>
+                  ))}
+                </li>
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <div>
-        <input
-          value={money}
-          onChange={handleInput}
-          type="number"
-          placeholder="Put the money you got"
-        />
-      </div>
-      <h2>
-        You can get {money / cost} for {money} dollars
-      </h2>
     </div>
   );
 }
